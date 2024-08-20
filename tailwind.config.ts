8df8170267cf5652484730d4ddf21d355bd73754
fhdzleon <fhdzleon@gmail.com>
2024-08-19 23:47:27 -0600

    colors add tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        bpgray: "#706f6f",
+        secundary: "#2b4168",
+        acent: "#5eba98",
+      },
     },
   },
   plugins: [],
