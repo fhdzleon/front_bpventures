162946ffd4c036500108b427fe4bbd68476717bc
fhdzleon <fhdzleon@gmail.com>
2024-08-20 14:54:33 -0600

    recuperando archivos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["i.ibb.co"],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
