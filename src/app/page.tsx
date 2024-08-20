f25fa14719e1b8b3bce0374d96876c63243564bb
fhdzleon <fhdzleon@gmail.com>
2024-08-20 14:20:08 -0600

    estructura lista

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Test from "@/components/test";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>bpVentures</h1>
-      <Test />
     </main>
   );
 }
