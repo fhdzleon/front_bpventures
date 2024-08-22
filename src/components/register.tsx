79bd619f95d214e1699bdcb73a3d1f265e783c04
giovanna <giovi92@hotmail.com>
2024-08-22 20:01:06 -0300

    create user

---
+++ src/components/register.tsx
@@ -0,0 +1,45 @@
+// import React from 'react';
+
+// const LoginPage: React.FC = () => {
+//   return (
+//     <div className="flex h-screen">
+//       <div className="flex-1 bg-cover bg-right relative" style={{ backgroundImage: "url('./public/img/register-desktop-bg.svg')" }}>
+//         <img
+//           className="absolute top-2.5 left-2.5 w-40 h-auto"
+//           src="./public/img/logo-bpventures.svg"
+//           alt="Logo"
+//         />
+//         <img
+//           className="absolute top-72 left-12 w-[500px] h-auto hidden sm:block"
+//           src="./public/img/avatar.svg"
+//           alt="People"
+//         />
+//       </div>
+//       <div className="flex-1 flex justify-center items-center">
+//         <div className="w-4/5 max-w-sm p-5 bg-white shadow-lg rounded-lg">
+//           <h2 className="mb-5 text-center">Login</h2>
+//           <input
+//             className="w-full p-2.5 mb-2.5 border border-gray-300 rounded-md"
+//             type="text"
+//             placeholder="Username"
+//             required
+//           />
+//           <input
+//             className="w-full p-2.5 mb-2.5 border border-gray-300 rounded-md"
+//             type="password"
+//             placeholder="Password"
+//             required
+//           />
+//           <button
+//             className="w-full p-2.5 bg-blue-600 text-white rounded-md hover:bg-blue-800"
+//             type="submit"
+//           >
+//             Login
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
