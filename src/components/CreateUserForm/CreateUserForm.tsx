5acb99f115c1f2c3b25f296b557cd7de2c84e22a
giovanna <giovi92@hotmail.com>
2024-08-26 20:05:23 -0300

    new change

--- src/components/CreateUserForm/CreateUserForm.tsx
+++ src/components/CreateUserForm/CreateUserForm.tsx
@@ -1,8 +1,7 @@
-"use client";	
+"use client";
 import { RegisterUser } from "@/helpers/auth.helper";
 import React, { useState } from "react";
 import { Toaster, toast } from "sonner";
-import InputField from "../FormComponent/InputField";
 import Button from "../FormComponent/Button";
 
 interface ICreateUser {
@@ -13,6 +12,16 @@ interface ICreateUser {
   Position: string;
 }
 
+const generateRandomPassword = (length: number = 12): string => {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:',.<>?/";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+};
+
 export const CreateUserForm: React.FC = () => {
   const [formData, setFormData] = useState<ICreateUser>({
     email: "",
@@ -21,10 +30,9 @@ export const CreateUserForm: React.FC = () => {
     LastName: "",
     Position: "",
   });
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
@@ -32,123 +40,82 @@ export const CreateUserForm: React.FC = () => {
     });
   };
 
+  const handlePasswordGeneration = () => {
+    const newPassword = generateRandomPassword();
+    setFormData({
+      ...formData,
+      password: newPassword,
+    });
+  };
+
+  const handlePasswordVisibilityToggle = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     try {
       await RegisterUser(formData);
-      toast.success("¡Usuario Registrado exitosamente!");
-
+      toast.success("¡Usuario registrado exitosamente!");
     } catch (error: any) {
-      console.error('Error registering user:', error);
+      console.error("Error al registrar usuario:", error);
       toast.error(error.message);
     }
   };
 
+  const fieldLabels: { [key: string]: string } = {
+    email: "Correo electrónico",
+    password: "Contraseña",
+    Names: "Nombres",
+    LastName: "Apellidos",
+    Position: "Puesto",
+  };
+
   return (
-    <>
-    <form onSubmit={handleSubmit} className="space-y-6 bg-gradient-to-b from-white via-[#d6dbdc] to-white dark:from-black dark:via-black dark:to-black p-6 rounded-lg shadow-lg text-[color:rgb(var(--foreground-rgb))]">
-       <Toaster richColors />
-
-      {/* <div>
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg text-balance dark:bg-gray-800 dark:border-gray-700"
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg text-balance dark:bg-gray-800 dark:border-gray-700"
-        />
-      </div>
-      <div>
-        <label htmlFor="Names" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Names</label>
-        <input
-          type="text"
-          id="Names"
-          name="Names"
-          value={formData.Names}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg text-balance dark:bg-gray-800 dark:border-gray-700"
-        />
-      </div>
-      <div>
-        <label htmlFor="LastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Last Name</label>
-        <input
-          type="text"
-          id="LastName"
-          name="LastName"
-          value={formData.LastName}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg text-balance dark:bg-gray-800 dark:border-gray-700"
-        />
-      </div>
-      <div>
-        <label htmlFor="Position" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100">Position</label>
-        <input
-          type="text"
-          id="Position"
-          name="Position"
-          value={formData.Position}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg text-balance dark:bg-gray-800 dark:border-gray-700"
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full py-3 mt-6 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        Create User
-      </button> */}
-        <div className="w-4/5 max-w-md p-5 bg-white shadow-lg rounded-lg">
-        <h2 className="font-futura text-center">Crear Usuario</h2>
-
-        <form className="form-apply" onSubmit={handleSubmit}>
-          {["email", "password", "Names", "LastName", "Position"].map((field, index) => (
-            <>
-              <label htmlFor={field} className="label-apply">
-              {field}
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <Toaster richColors />
+      <form className="form-apply" onSubmit={handleSubmit}>
+        <h2 className="text-center text-[1.2rem] mb-6">Creación de Usuario</h2>
+        {Object.keys(formData).map((field, index) => (
+          <div key={index} className="mb-4">
+            <label htmlFor={field} className="label-apply">
+              {fieldLabels[field]}
             </label>
-            <input
-              key={index}
-             className="relative font-futura border-[0.5px] border-gray-300 appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent"
-              type={field === "password" ? "password" : "text"}
-              name={field}
-              // value={userData[field as keyof User]}
-              // onChange={handleInputChange}
-              placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-              required
-            />
-             </>
-          ))}
-          
+            {field === "Position" ? (
+              <select id={field} name={field} className="input-apply" value={formData[field as keyof ICreateUser]} onChange={handleChange} required>
+                <option value="gerente">Gerente</option>
+                <option value="supervisor">Supervisor</option>
+                <option value="operario">Operario</option>
+              </select>
+            ) : (
+              <div className="relative">
+                <input
+                  className="input-apply"
+                  type={field === "password" && !passwordVisible ? "password" : "text"}
+                  name={field}
+                  value={field === "password" ? formData.password : formData[field as keyof ICreateUser]}
+                  onChange={handleChange}
+                  placeholder={fieldLabels[field]}
+                  required
+                />
+                {field === "password" && (
+                  <button type="button" onClick={handlePasswordVisibilityToggle} className="absolute inset-y-0 right-0 px-3 py-2 text-gray-500">
+                    {passwordVisible ? "Ocultar" : "Mostrar"}
+                  </button>
+                )}
+              </div>
+            )}
+            {field === "password" && (
+              <Button onClick={handlePasswordGeneration} type="button">
+                Generar Contraseña
+              </Button>
+            )}
+          </div>
+        ))}
 
-          
-          <Button type="submit">Crear Usuario</Button>
-        </form>
-      </div>
-      
-    </form>
-    
-    </>
-
-
+        <Button type="submit">Crear Usuario</Button>
+      </form>
+    </div>
   );
 };
 
--- src/components/CreateUserForm/UpdateUser.tsx
+++ src/components/CreateUserForm/UpdateUser.tsx
@@ -5,6 +5,7 @@ import { GetUserById, UpdateUser } from "@/helpers/auth.helper";
 import React, { useEffect, useState } from "react";
 import "../../styles/form-style.css";
 import Button from "../FormComponent/Button";
+import { Toaster, toast } from "sonner";
 
 interface User {
   email: string;
@@ -12,15 +13,15 @@ interface User {
   Names: string;
   LastName: string;
   Position: string;
+  oldPassword?: string; // Añadido para la contraseña antigua
 }
 interface Props {
   id: number;
 }
+
 const UpdateUserComponent: React.FC<Props> = (props) => {
   const { id } = props;
-  console.log(id);
-  const { user } = useAuth();
-  const userId = id; // Considera hacer esto dinámico si es necesario
+  const userId = id;
 
   const [userData, setUserData] = useState<User>({
     email: "",
@@ -28,76 +29,117 @@ const UpdateUserComponent: React.FC<Props> = (props) => {
     Names: "",
     LastName: "",
     Position: "",
+    oldPassword: "", // Añadido para la contraseña antigua
   });
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+  const [oldPasswordVisible, setOldPasswordVisible] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const res = await GetUserById(userId.toString());
-        setUserData(res); // Aquí se actualiza el estado con los datos obtenidos
+        setUserData(res);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
 
     fetchUser();
-  }, []);
+  }, [userId]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setUserData((prevUser) => ({ ...prevUser, [name]: value }));
   };
 
-  const userData2 = {
-    email: "newemail@example.com",
-    Names: "John Updated",
-    LastName: "Doe Updated",
-    Position: "Manager",
-    mfaEnabled: false,
-    active: true,
+  const handlePasswordVisibilityToggle = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleOldPasswordVisibilityToggle = () => {
+    setOldPasswordVisible(!oldPasswordVisible);
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       await UpdateUser(userData, userId.toString());
-      alert("User updated successfully");
-    } catch (error) {
-      console.error("Error updating user:", error);
+      toast.success("Usuario actualizado exitosamente!");
+    } catch (error:any) {
+      console.error("Error al actualizar el usuario:", error);
+      toast.error(error.message);
+
     }
   };
 
+  const fieldLabels: { [key: string]: string } = {
+    email: "Correo electrónico",
+    password: "Contraseña",
+    oldPassword: "Contraseña antigua", // Etiqueta para el nuevo campo
+    Names: "Nombres",
+    LastName: "Apellidos",
+    Position: "Puesto",
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      
-      <div className="w-4/5 max-w-md p-5 bg-white shadow-lg rounded-lg">
-        <h2 className="font-futura text-center">Editar Usuario</h2>
+    <div className="flex w-full justify-center">
+      <Toaster richColors />
+      <div className="md:grid md:grid-cols-2 p-5 bg-white shadow-lg rounded-lg">
+        <div className="col-span-1 font-futura">
+          <h2 className="text-start mt-10 text-2xl text-[#2B4168]">Editar Usuario</h2>
+          <p className="text-black/50">Si deseas editar los datos de tu usuario, puedes llenar los siguientes campos:</p>
+        </div>
 
-        <form className="form-apply" onSubmit={handleSubmit}>
-          {["email", "password", "Names", "LastName", "Position"].map((field, index) => (
-            <>
-              <label htmlFor={field} className="label-apply">
-              {field}
-            </label>
-            <input
-              key={index}
-             className="relative font-futura border-[0.5px] border-gray-300 appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent"
-              type={field === "password" ? "password" : "text"}
-              name={field}
-              value={userData[field as keyof User]}
-              onChange={handleInputChange}
-              placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-              required
-            />
-             </>
-          ))}
-          
+        <div className="col-span-1">
+          <form className="form-apply" onSubmit={handleSubmit}>
+            {["email", "oldPassword", "password", "Names", "LastName", "Position"].map((field, index) => (
+              <React.Fragment key={index}>
+                <label htmlFor={field} className="label-apply">
+                  {fieldLabels[field]}
+                </label>
+                {field === "Position" ? (
+                  <select
+                    id={field}
+                    name={field}
+                    className="relative font-futura border-[0.5px] border-gray-300 appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent"
+                    value={userData[field as keyof User]}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    <option value="gerente">Gerente</option>
+                    <option value="supervisor">Supervisor</option>
+                    <option value="operario">Operario</option>
+                  </select>
+                ) : (
+                  <div className="relative">
+                    <input
+                      className="relative font-futura border-[0.5px] border-gray-300 appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent"
+                      type={(field === "password" && !passwordVisible) || (field === "oldPassword" && !oldPasswordVisible) ? "password" : "text"}
+                      name={field}
+                      value={userData[field as keyof User]}
+                      onChange={handleInputChange}
+                      placeholder={fieldLabels[field]}
+                      required
+                    />
+                    {field === "password" && (
+                      <button type="button" onClick={handlePasswordVisibilityToggle} className="absolute inset-y-0 right-0 px-3 py-2 text-gray-500">
+                        {passwordVisible ? "Ocultar" : "Mostrar"}
+                      </button>
+                    )}
+                    {field === "oldPassword" && (
+                      <button type="button" onClick={handleOldPasswordVisibilityToggle} className="absolute inset-y-0 right-0 px-3 py-2 text-gray-500">
+                        {oldPasswordVisible ? "Ocultar" : "Mostrar"}
+                      </button>
+                    )}
+                  </div>
+                )}
+              </React.Fragment>
+            ))}
 
-
-          <Button type="submit">Guardar Cambios</Button>
-        </form>
+            <Button type="submit">Guardar Cambios</Button>
+          </form>
+        </div>
       </div>
-
     </div>
   );
 };
