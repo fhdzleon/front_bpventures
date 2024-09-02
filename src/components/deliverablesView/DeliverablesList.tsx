2f4946e7b872e5796a11be55fc6551b99e2ffb54
fhdzleon <fhdzleon@gmail.com>
2024-09-02 00:11:04 -0600

    permisos para dmin habilitados

--- src/components/deliverablesView/DeliverablesList.tsx
+++ src/components/deliverablesView/DeliverablesList.tsx
@@ -24,7 +24,6 @@ const DeliverablesList = () => {
     setOpenPanel(openPanel === fileId ? null : fileId);
   };
 
-
   useEffect(() => {
     const fetchDeliverables = async () => {
       try {
@@ -220,7 +219,8 @@ const DeliverablesList = () => {
                       <td>
                         <div className="grid grid-cols-3 justify-center justify-items-center">
                           {/* Si el usuario es owner, puede hacer todo */}
-                          {deliverable.permissionTypes?.includes("owner") ? (
+                          {deliverable.permissionTypes?.includes("owner") ||
+                          userData.isAdmin ? (
                             <>
                               <DownloadDeliverable
                                 id={deliverable.id}
@@ -267,7 +267,8 @@ const DeliverablesList = () => {
                       </td>
                     )}
 
-                    {(deliverable.permissionTypes?.includes("owner") || userData?.isAdmin) && 
+                    {(deliverable.permissionTypes?.includes("owner") ||
+                      userData?.isAdmin) &&
                     !deliverable.deliverableIsFolder ? (
                       <td className="py-4 px-6 text-sm text-gray-700 relative">
                         <button
