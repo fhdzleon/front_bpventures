a55bc52149fe92624da9baa9d2dea77509eba1d7
vanesabucher <buchervanesa@gmail.com>
2024-09-12 17:49:24 -0300

    socket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { SocketProvider } from "@/context/WebSocket";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+   
+      <body className={inter.className}> 
+        <SocketProvider>
         <AuthProvider>{children}</AuthProvider>
+        </SocketProvider>
       </body>
     </html>
   );
