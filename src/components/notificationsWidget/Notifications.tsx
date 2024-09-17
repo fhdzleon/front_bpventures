a00c081cf3df64feb964e93c859c4769e1739fae
fhdzleon <fhdzleon@gmail.com>
2024-09-16 19:18:50 -0600

    notifications whit back

--- src/components/notificationsWidget/Notifications.tsx
+++ src/components/notificationsWidget/Notifications.tsx
@@ -1,26 +1,53 @@
-import React from "react";
+"use client";
 import { usersData } from "@/helpers/UsersData";
+import React, { useState, useEffect } from "react";
 import NotificationCard from "./NotificationCard";
+import { Notification } from "@/interfaces/notifications";
 
 const Notifications: React.FC = () => {
+  const [notificationsData, setNotificationData] = useState<Notification[]>([]);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/notifications`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setNotificationData(data);
+        } else {
+          throw new Error("No se cargaron las notificaciones");
+        }
+      } catch (error) {
+        console.error("Error al cargar las notificaciones");
+      }
+    };
+    fetchNotifications();
+  }, []);
+
   return (
     <div className="px-10">
       <h2 className="text-4xl font-futura mb-6 text-secundary">
         Actividad reciente
       </h2>
       <div className="space-y-0">
-        {usersData.map((notification, index) => (
-          <NotificationCard
-            key={index}
-            note={notification.note}
-            notificationType={notification.notificationType}
-            impactedUser={notification.impactedUser}
-            triggerUser={notification.triggerUser}
-            deliverable={notification.deliverable}
-            invoice={notification.invoice}
-            createdAt={notification.createdAt}
-          />
-        ))}
+        {notificationsData.length > 0 ? (
+          notificationsData.map((notification, index) => (
+            <NotificationCard
+              key={index}
+              note={notification.note}
+              notificationType={notification.notificationType}
+              impactedUser={notification.impactedUser}
+              triggerUser={notification.triggerUser}
+              deliverable={notification.deliverable}
+              invoice={notification.invoice}
+              createdAt={notification.createdAt}
+            />
+          ))
+        ) : (
+          <span>No hay actividad registrada</span>
+        )}
       </div>
     </div>
   );
--- src/context/WebSocket.tsx
+++ src/context/WebSocket.tsx
@@ -1,6 +1,12 @@
-'use client'
-import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-import { io, Socket } from 'socket.io-client'
+"use client";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { io, Socket } from "socket.io-client";
 
 interface ISocketContext {
   socket: Socket | null;
@@ -14,17 +20,16 @@ const SocketContext = createContext<ISocketContext | undefined>(undefined);
 export const useSocket = (): Socket | null => {
   const context = useContext(SocketContext);
   if (context === undefined) {
-    throw new Error('useSocket must be used within a SocketProvider');
+    throw new Error("useSocket must be used within a SocketProvider");
   }
   return context.socket;
 };
-
 
 export const SocketProvider = ({ children }: SocketProviderProps) => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const socketIo = io('http://127.0.0.1:5000'); // Cambia la URL según la configuración de tu servidor
+    const socketIo = io(`${process.env.NEXT_PUBLIC_API_URL}`); // Cambia la URL según la configuración de tu servidor
 
     setSocket(socketIo);
 
