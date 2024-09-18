94719c755ae0b39b409f4ad4c2d005c8d2879329
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-09-17 23:31:15 -0500

    cambios de vanesa mas audio

--- src/components/ButtonAlerts/ButtonAlerts.tsx
+++ src/components/ButtonAlerts/ButtonAlerts.tsx
@@ -138,7 +138,6 @@ import { useEffect, useState } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { io } from "socket.io-client";
 
-
 const formatRelativeTime = (dateString: string | number | Date) => {
   const date = new Date(dateString);
   const now = new Date();
@@ -151,26 +150,26 @@ const formatRelativeTime = (dateString: string | number | Date) => {
 
   const rtf = new Intl.RelativeTimeFormat("es", { numeric: "auto" });
 
-  if (days > 30) {
-    return date.toLocaleDateString("es-ES", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  } else if (days >= 7) {
-    return rtf.format(-weeks, "week");
-  } else if (days > 0) {
-    return rtf.format(-days, "day");
-  } else if (hours > 0) {
-    return rtf.format(-hours, "hour");
-  } else if (minutes > 0) {
-    return rtf.format(-minutes, "minute");
-  } else {
-    return rtf.format(-seconds, "second");
-  }
+  // if (days > 30) {
+  //   return date.toLocaleDateString("es-ES", {
+  //     weekday: "long",
+  //     year: "numeric",
+  //     month: "long",
+  //     day: "numeric",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //   });
+  // } else if (days >= 7) {
+  //   return rtf.format(-weeks, "week");
+  // } else if (days > 0) {
+  //   return rtf.format(-days, "day");
+  // } else if (hours > 0) {
+  //   return rtf.format(-hours, "hour");
+  // } else if (minutes > 0) {
+  //   return rtf.format(-minutes, "minute");
+  // } else {
+  //   return rtf.format(-seconds, "second");
+  // }
 };
 
 const Notifications = () => {
@@ -204,7 +203,7 @@ const Notifications = () => {
   useEffect(() => {
     if (!userId) return;
 
-    const socket = io(`${process.env.NEXT_PUBLIC_API_URL}/notifications`, {
+    const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`, {
       query: { userId: userId },
     });
 
@@ -212,6 +211,7 @@ const Notifications = () => {
       console.log("Nueva notificación:", data);
       setHasNewNotification(true);
       setNotifications((prev) => [data, ...prev]);
+      playSound();
     });
 
     return () => {
@@ -221,6 +221,15 @@ const Notifications = () => {
     };
   }, [userId]);
 
+  function playSound() {
+    const audio = new Audio();
+    audio.src = '/sounds/notification.mp3'; // Formato MP3 para mayor compatibilidad
+    audio.onerror = () => {
+      audio.src = '/sounds/notification.ogg'; // Fallo con MP3, usa OGG
+    };
+    audio.play();
+  }
+  
   const toggleNotifications = () => {
     setIsOpen(!isOpen);
     if (hasNewNotification) {
@@ -230,33 +239,48 @@ const Notifications = () => {
 
   return (
     <div className="relative">
-       <button
-    onClick={toggleNotifications}
-    className={`bg-acent flex items-center text-white p-2 rounded-full focus:outline-none hover:bg-acent transition ${hasNewNotification ? 'bg-red-500' : ''}`}
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-      />
-    </svg>
-    {/* Texto visible solo en pantallas grandes */}
-    <span className="hidden md:inline ml-2">Notificaciones</span>
-    {/* Indicador de notificaciones */}
-    {hasNewNotification && (
-      <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-2 py-1">
-        !
-      </span>
-    )}
-  </button>
+      <button
+        onClick={toggleNotifications}
+        className={`bg-acent flex items-center text-white p-2 rounded-full focus:outline-none hover:bg-acent transition ${
+          hasNewNotification ? "bg-acent" : ""
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+          />
+        </svg>
+        {/* Texto visible solo en pantallas grandes */}
+        <span className="hidden md:inline ml-2">Notificaciones</span>
+        {/* Indicador de notificaciones */}
+        {hasNewNotification && (
+          <span className="absolute -top-3 -right-3 bg-red-400 text-white text-xs rounded-full px-2 py-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+              />
+            </svg>
+          </span>
+        )}
+      </button>
 
       {isOpen && (
         <div className="absolute top-12 right-0 bg-white w-96 border border-gray-200 rounded-lg shadow-lg transition-transform duration-300 ease-in-out">
@@ -295,9 +319,9 @@ const Notifications = () => {
                       </strong>{" "}
                       acaba de: <br />
                       <strong> {notification?.notificationType?.name}</strong>
-                      <br />      {notification?.deliverable
-                   ?
-                         `${notification.deliverable.name}`
+                      <br />{" "}
+                      {notification?.deliverable
+                        ? `${notification.deliverable.name}`
                         : notification?.invoice?.number
                         ? ` ${notification.invoice.number}`
                         : ""}{" "}
@@ -309,7 +333,7 @@ const Notifications = () => {
                       </strong>
                     </p>
                     <p className="text-gray-500 text-xs">
-                    {formatRelativeTime(notification?.createdAt)}
+                      {/* {formatRelativeTime(notification?.createdAt)} */}
                     </p>
                   </div>
                 </li>
