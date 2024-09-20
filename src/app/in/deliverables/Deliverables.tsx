133e3d098d51f96d6f87324a999ac26c1c948de9
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-09-20 16:55:51 -0500

    fixing mis archivos anchura de la tabla

--- src/app/in/deliverables/Deliverables.tsx
+++ src/app/in/deliverables/Deliverables.tsx
@@ -5,7 +5,7 @@ import DeliverablesList from "@/components/deliverablesView/DeliverablesList";
 const Deliverables = () => {
   return (
     <div className="container max-w-full">
-      <div className="container mx-20 py-6 max-w-[1400px] font-futura">
+      <div className=" mx-20 py-6 max-w-[1400px] font-futura">
         <h1 className="text-4xl font-futura  text-secundary">Mis Archivos</h1>
         <DeliverablesList />
       </div>
